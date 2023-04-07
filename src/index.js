2d9ac5b59e0ee172f9cd63ee6f5b38c02fe5d789
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-08 01:00:22 +0300

    add materialUi

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -12,3 +17,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
+
+// npm install @mui/icons-material
+
+// [Чтобы обеспечить правильный рендеринг и сенсорное масштабирование для всех устройств, добавьте метатег адаптивного окна просмотра к элементу <head>:]
+// <meta name="viewport" content="initial-scale=1, width=device-width" />
