a90a41951ea5682dc80cd3cd6ea4b13af7df9242
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-09 15:19:52 +0300

    1

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/Layout';
 
 const HomePage = lazy(() => import('../pages/Home'));
-// const RegisterPage = lazy(() => import('../pages/Register'));
+const RegisterPage = lazy(() => import('../pages/Register'));
 const LoginPage = lazy(() => import('../pages/Login'));
 // const ContactsPage = lazy(() => import('../pages/Contacts'));
 
@@ -14,7 +14,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route index element={<HomePage />}>
-          {/* <Route path="register" element={<RegisterPage />} /> */}
+          <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />
           {/* <Route path="contacts" element={<ContactsPage />} /> */}
         </Route>
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,37 +1,14 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-// import Link from '@mui/material/Link';
-// import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Box,
+  Typography,
+  Container,
+} from '@mui/material';
+
 import HowToRegIcon from '@mui/icons-material/HowToReg';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// function Copyright(props) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://mui.com/">
-//         Phonebook
-//       </Link>
-//       {new Date().getFullYear()}
-//     </Typography>
-//   );
-// }
-
-// const theme = createTheme();
 
 export const RegisterForm = () => {
   const handleSubmit = event => {
@@ -44,7 +21,6 @@ export const RegisterForm = () => {
   };
 
   return (
-    // <ThemeProvider theme={theme}>
     <>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -56,9 +32,7 @@ export const RegisterForm = () => {
             alignItems: 'center',
           }}
         >
-          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}> */}
           <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            {/* <LockOutlinedIcon /> */}
             <HowToRegIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
@@ -99,10 +73,7 @@ export const RegisterForm = () => {
               id="password"
               autoComplete="current-password"
             />
-            {/* <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            /> */}
+
             <Button
               type="submit"
               fullWidth
@@ -111,23 +82,9 @@ export const RegisterForm = () => {
             >
               Register
             </Button>
-            {/* <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid> */}
           </Box>
         </Box>
-        {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
       </Container>
-      {/* </ThemeProvider> */}
     </>
   );
 };
