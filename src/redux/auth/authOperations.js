762facc0ed975d8779fd51e6de1fff1aa7923bb4
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-09 21:36:28 +0300

    *

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -16,27 +16,3 @@ export const register = createAsyncThunk(
     }
   }
 );
-
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async ({ name, phone }, { rejectWithValue }) => {
-//     try {
-//       const resp = await axios.post('/contacts', { name, phone });
-//       return resp.data;
-//     } catch (err) {
-//       return rejectWithValue(err.message);
-//     }
-//   }
-// );
-
-// export const deleteContact = createAsyncThunk(
-//   'contacts/deleteContact ',
-//   async (id, { rejectWithValue }) => {
-//     try {
-//       const resp = await axios.delete(`/contacts/${id}`);
-//       return resp.data;
-//     } catch (err) {
-//       return rejectWithValue(err.message);
-//     }
-//   }
-// );
