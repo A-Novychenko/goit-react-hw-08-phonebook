4ff8e64d80b3a6d96cb379cac5ba55ba1c297ad0
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-09 15:21:30 +0300

    2

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,8 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<HomePage />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />
           {/* <Route path="contacts" element={<ContactsPage />} /> */}
