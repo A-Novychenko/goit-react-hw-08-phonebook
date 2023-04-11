8e4b92958457103a252752222243feefafd3c157
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-11 11:08:12 +0300

    test +reg

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 import { refreshUser } from 'redux/auth/authOperations';
 
 const HomePage = lazy(() => import('../pages/Home'));
-// const RegisterPage = lazy(() => import('../pages/Register'));
+const RegisterPage = lazy(() => import('../pages/Register'));
 const LoginPage = lazy(() => import('../pages/Login'));
 // const ContactsPage = lazy(() => import('../pages/Contacts'));
 
@@ -21,7 +21,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          {/* <Route path="/register" element={<RegisterPage />} /> */}
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           {/* <Route path="/contacts" element={<ContactsPage />} /> */}
         </Route>
